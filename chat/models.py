dd48f2eb6f26a6dfca252f91b0d13494e558e733
Kristian Yendrek <kristianendreck4@gmail.com>
2024-01-11 15:13:48 +0200

    Added user, thread, message models

--- chat/models.py
+++ chat/models.py
@@ -1,1 +1,25 @@
+from django.conf import settings
 from django.db import models
+
+
+class Thread(models.Model):
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="threads"
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name="messages",
+        null=True,
+    )
+    text = models.TextField()
+    thread = models.ForeignKey(
+        Thread, on_delete=models.CASCADE, related_name="messages"
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField()
