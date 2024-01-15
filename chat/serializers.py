de1b6f6997cda53394cca74478597b7c579d7045
Kristian Yendrek <kristianendreck4@gmail.com>
2024-01-15 15:50:44 +0200

    Created ThreadList view and Message, ThreadList serializers

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 
-from chat.models import Thread
+from chat.models import Thread, Message
 from user.serializers import UserSerializer
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ("id", "sender", "text", "thread", "created", "is_read")
 
 
 class ThreadSerializer(serializers.ModelSerializer):
@@ -9,4 +17,12 @@ class ThreadSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Thread
-        fields = ['id', 'participants', 'created', 'updated']
+        fields = ('id', 'participants', 'created', 'updated')
+
+
+class ThreadListSerializer(ThreadSerializer):
+    last_message = MessageSerializer(read_only=True)
+
+    class Meta:
+        model = Thread
+        fields = ('id', 'participants', 'created', 'updated', 'last_message')
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
 
-# Create your views here.
+from chat.models import Thread
+from chat.serializers import ThreadListSerializer
+
+
+class ThreadList(generics.ListAPIView):
+    queryset = Thread.objects.all()
+    serializer_class = ThreadListSerializer
+
+    def get_queryset(self):
+        threads = self.queryset.filter(user=self.request.user)
+
+        for thread in threads:
+            last_message = thread.messages.last()
+            thread.last_message = last_message
+
+        return threads
+
+
